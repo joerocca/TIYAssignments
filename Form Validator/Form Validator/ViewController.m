3c8dc12b9d4306ddf9336053183d1c63fb6a463f
Joe Rocca <jprocca813@gmail.com>
2015-03-10 21:29:50 -0400

    Updated textFieldShouldReturn Method.

--- Form Validator/Form Validator/ViewController.m
+++ Form Validator/Form Validator/ViewController.m
@@ -31,7 +31,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"Form Validator";
-//    validator = [[FormValidator alloc] init];
+    validator = [[FormValidator alloc] init];
     
     // Do any additional setup after loading the view, typically from a nib.
 }
@@ -41,61 +41,56 @@
     // Dispose of any resources that can be recreated.
 }
 
-
-
+#pragma mark - UITextField delegate
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     BOOL rc = NO;
+    UITextField *nextTextField = nil;
     
-    if (textField == self.nameTextField)
+    if (![textField.text isEqualToString:@""])
     {
-        rc = [validator validateName:self.nameTextField.text];
+        if (textField == self.nameTextField)
+        {
+            rc = [validator validateName:textField.text];
+            nextTextField = self.addressTextField;
+        }
+        else if (textField == self.addressTextField)
+        {
+            rc = [validator validateAddress:textField.text];
+            nextTextField = self.cityTextField;
+        }
+        else if (textField == self.cityTextField)
+        {
+            rc = YES;
+            nextTextField = self.stateTextField;
+        }
+        else if (textField == self.stateTextField)
+        {
+            rc = [validator validateState:textField.text];
+            nextTextField = self.zipCodeTextField;
+        }
+        else if (textField == self.zipCodeTextField)
+        {
+            rc = [validator validateZipCode:textField.text];
+            nextTextField = self.phoneNumberTextField;
+        }
+        else if (textField == self.phoneNumberTextField)
+        {
+            rc = [validator validatePhoneNumber:textField.text];
+        }
         
-        
-    }
-    
-    if (textField == self.cityTextField)
-    {
-        rc = [validator validateCity:self.cityTextField.text];
-    }
-    
-    if (textField == self.zipCodeTextField)
-    {
-        rc = [validator validateZipCode:self.zipCodeTextField.text];
-    }
-    
-    if (textField == self.stateTextField)
-    {
-        rc = [validator validateState:self.zipCodeTextField.text];
-    }
-    
-    if(textField == self.phoneNumberTextField)
-    {
-        rc = [validator validatePhoneNumber:self.phoneNumberTextField.text];
-    }
-    
-    if (textField == self.addressTextField)
-    {
-        rc = [validator validateAddress:self.addressTextField.text];
-    }
-    
-    
-    if (rc)
-    {
-        [textField resignFirstResponder];
+        if (rc)
+        {
+            [textField resignFirstResponder];
+            if (nextTextField)
+            {
+                [nextTextField becomeFirstResponder];
+            }
+        }
     }
     
     return rc;
 }
-
-
-
-
-
-
-
-
-
 
 @end
