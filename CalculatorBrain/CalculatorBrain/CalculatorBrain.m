b31485353a0909cf29b5f4e0022f181e3095bf0e
Joe Rocca <jprocca813@gmail.com>
2015-03-10 21:36:00 -0400

    Implemented methods.

--- CalculatorBrain/CalculatorBrain/CalculatorBrain.m
+++ CalculatorBrain/CalculatorBrain/CalculatorBrain.m
@@ -8,32 +8,29 @@
 
 #import "CalculatorBrain.h"
 
+
+
 @implementation CalculatorBrain
 
 
-- (instancetype) init
+
+-(instancetype)init
 {
-    self = [super init];
+    self = [super init];                //overiding init of super class (NSObject)
     if (self)
     {
+        
         _operand1String = [[NSMutableString alloc] init];
-        _operand2String = [[NSMutableString alloc] init];
+        _operand2String = [[NSMutableString alloc] init];       //_ is form for instance variables when overriding init
         _operand1 = 0.0f;
         _operand2 = 0.0f;
         _operatorType = OperatorTypeNone;
         _userIsTypingANumber = NO;
-        
     }
     
     return self;
 }
 
-
-//-(void)hello
-//{
-//self.operand1 = [self.operand1String floatValue];
-//self.operand2 = [self.operand2String floatValue];
-//}
 
 
 
@@ -56,11 +53,64 @@
 }
 
 
+
+
+- (NSString *) addDecimalPoint
+{
+    
+    NSString *rc;
+    
+    if ([self.operand1String isEqualToString:@""])
+    {
+        self.operand1String = [@"0." mutableCopy];
+        rc = self.operand1String;
+    }
+    
+    else if (self.operatorType == OperatorTypeNone)
+    {
+        if (![self.operand1String containsString:@"."])
+        {
+            [self.operand1String appendString:@"."];
+        }
+        
+        
+        
+        rc = self.operand1String;
+        
+    }
+    
+    
+    else if ([self.operand2String isEqualToString:@""])
+    {
+        self.operand2String = [@"0." mutableCopy];
+        rc = self.operand2String;
+        
+    }
+    else if (!self.operatorType == OperatorTypeNone)
+    {
+        if (![self.operand2String containsString:@"."])
+        {
+            [self.operand2String appendString:@"."];
+        }
+        
+        rc = self.operand2String;
+        
+    }
+    
+  
+    return rc;
+}
+
+
+
+
+
+
 - (float) useOperator
 {
     
     
-
+    
     float returnValue;
     switch (self.operatorType)
     {
@@ -91,38 +141,7 @@
             returnValue = self.operand1 / self.operand2;
             break;
             
-        case OperatorTypePercent:
             
-            self.operand1 = [self.operand1String floatValue];
-            self.operand2 = [self.operand2String floatValue];
-            if (_operatorType)
-            {
-                returnValue = self.operand1 * 0.01;
-            }
-            else
-            {
-                returnValue = self.operand2 * 0.01;
-            }
-            break;
-            
-        case OperatorTypeSignChanger:
-            
-            self.operand1 = [self.operand1String floatValue];
-            self.operand2 = [self.operand2String floatValue];
-            if (_operatorType)
-            {
-                returnValue = self.operand1 * -1;
-            }
-            else
-            {
-                returnValue = self.operand2 * -1;
-            }
-            break;
-        
-        
-        
-            
-        
         default:
             break;
     }
@@ -134,6 +153,54 @@
 
 
 
+- (float) findPercent;
+{
+    float rc;
+    self.operand1 = [self.operand1String floatValue];
+    self.operand2 = [self.operand2String floatValue];
+    
+    if (self.operatorType == OperatorTypeNone)
+    {
+        float percentCalculation = self.operand1 * .01;
+        self.operand1String = [NSMutableString stringWithFormat:@"%g", percentCalculation];
+        rc = percentCalculation;
+    }
+    
+    else if (self.operatorType != OperatorTypeNone)
+    {
+        float percentCalculation = self.operand2 * .01;
+        self.operand2String = [NSMutableString stringWithFormat:@"%g", percentCalculation];
+        rc = percentCalculation;
+        
+    }
+    
+    return rc;
+}
+
+
+
+- (float) changePositiveNegative
+{
+    float rc;
+    self.operand1 = [self.operand1String floatValue];
+    self.operand2 = [self.operand2String floatValue];
+    
+    if (self.operatorType == OperatorTypeNone)
+    {
+        float positiveNegative = self.operand1 * -1;
+        self.operand1String = [NSMutableString stringWithFormat:@"%g",positiveNegative];
+        rc = positiveNegative;
+    }
+    
+    else if (self.operatorType != OperatorTypeNone)
+    {
+        float positiveNegative = self.operand2 * -1;
+        self.operand2String = [NSMutableString stringWithFormat:@"%g",positiveNegative];
+        rc = positiveNegative;
+    }
+    
+    return rc;
+}
 
 
 
